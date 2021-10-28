8dc18958f2399c15313cac00c6b09b067e143cc9
Administrator <nguyenhuynhcongly17092000@gmail.com>
2021-10-28 23:23:02 +0700

    ly push code

--- Ly_Folder/cong_nghe_moi2/app/src/main/java/com/example/cong_nghe_moi/MainActivity.java
+++ Ly_Folder/cong_nghe_moi2/app/src/main/java/com/example/cong_nghe_moi/MainActivity.java
@@ -25,58 +25,41 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     List<Account> listAccount;
     RecyclerView recyclerView;
-    List<com.example.cong_nghe_moi.Friend> fiendList;
+    List<Friend> fiendList;
     List<Contact> listFriend;
+    MyChairAdapter myChairAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        listFriend = getDanhBaById(1);
-
+         getDanhBaById(1);
         setContentView(R.layout.acti_danhba);
         fiendList = new ArrayList<>();
-
-       Toast.makeText(MainActivity.this,"Độ dài chuỗi: "+listFriend.size(), Toast.LENGTH_LONG).show();
-      //  for(int i = 0; i< 5;i++){
-        for(Contact a : listFriend){
-            fiendList.add(new com.example.cong_nghe_moi.Friend(R.drawable._061334, a.getFriend().getUsername(),R.drawable._21014,R.drawable._762106));
-        }
-       // fiendList.add(new com.example.cong_nghe_moi.Friend(R.drawable._061334, "Tu thy",R.drawable._21014,R.drawable._762106));
-        //fiendList.add(new com.example.cong_nghe_moi.Friend(R.drawable._061334, "Tu thy",R.drawable._21014,R.drawable._762106));
-        //fiendList.add(new com.example.cong_nghe_moi.Friend(R.drawable._061334, "Tu thy",R.drawable._21014,R.drawable._762106));
-
-
         recyclerView = findViewById(R.id.recyclerView);
-        com.example.cong_nghe_moi.MyChairAdapter myChairAdapter = new com.example.cong_nghe_moi.MyChairAdapter(fiendList,this);
+        myChairAdapter = new MyChairAdapter(fiendList,this);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
         recyclerView.setAdapter(myChairAdapter);
-
-
-
     }
-
-
-    public List<Contact> getDanhBaById(int id) {
+    public void getDanhBaById(int id) {
         List<Contact> listContact = new ArrayList<>();
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.1.9:3000/danhba/"+id;
+        String url = "http://192.168.0.110:3000/danhba/"+id;
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
 
                 for (int i = 0; i < response.length(); i++) {
                     try {
+                        fiendList = new ArrayList<>();
                         JSONObject object = (JSONObject) response.get(i);
                         Gson gson = new Gson();
                         Contact p = gson.fromJson(String.valueOf(object), Contact.class);
-                        listContact.add(p);
-                        System.out.println(p.toString());
-                        Toast.makeText(MainActivity.this, p.toString(), Toast.LENGTH_LONG).show();
+
+                        fiendList.add(new Friend(R.drawable._061334, p.getFriend().getUsername(),R.drawable._21014,R.drawable._762106));
+                         myChairAdapter.notifyDataSetChanged();
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
-
-
             }
         }, error -> {
             Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
@@ -86,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
         queue.add(jsonArrayRequest);
         Toast.makeText(MainActivity.this,"Độ dài chuỗi function: "+listContact.size(), Toast.LENGTH_LONG).show();
-        return listContact;
+
+
     }
 }
