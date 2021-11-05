bd91ab969898d303b4f5156b4f0455fb7fb58e40
Le Van Toan <lvtoan.it2000@gmail.com>
2021-11-05 20:27:21 +0700

    Add layout,image va cac entity Android

---
+++ ANDROID-PROJECT/Android_finally/app/src/main/java/com/example/android_finally/User.java
@@ -0,0 +1,51 @@
+package com.example.android_finally;
+
+
+public class User {
+    private int id;
+    private String soDienThoai;
+    private String password;
+    private boolean enable;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", soDienThoai='" + soDienThoai + '\'' +
+                ", password='" + password + '\'' +
+                ", enable=" + enable +
+                '}';
+    }
+}
