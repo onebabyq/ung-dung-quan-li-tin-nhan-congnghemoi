ff8b9b77df83b953738621fa6ef4b68a411036dd
Le Van Toan <lvtoan.it2000@gmail.com>
2021-11-11 17:33:28 +0700

    document

--- ANDROID-PROJECT/AppChat/app/src/main/java/com/example/appchat/Activities/ChatActivity.java
+++ ANDROID-PROJECT/AppChat/app/src/main/java/com/example/appchat/Activities/ChatActivity.java
@@ -26,7 +26,7 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-import tech.gusavila92.websocketclient.WebSocketClient;
+//import tech.gusavila92.websocketclient.WebSocketClient;
 
 public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = ChatActivity.class.getSimpleName();
@@ -36,7 +36,7 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 
     private ChatAdapter mChatAdapter;
     private final List<String> mMessageList = new ArrayList<>();
-    private WebSocketClient webSocketClient;
+//    private WebSocketClient webSocketClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,7 +45,7 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
         mChatAdapter = new ChatAdapter(mMessageList, this);
         initViews();
 //        setListening();
-        createWebSocketClient();
+//        createWebSocketClient();
 
         mImgBtnDictionnary.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,76 +83,76 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
         mImgBtnSelect = (ImageButton) findViewById(R.id.imgBtnTuyChon);
     }
 
-    private void createWebSocketClient() {
-        URI uri;
-        try {
-            uri = new URI("ws://10.0.2.2:8080/websocket");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        webSocketClient = new WebSocketClient(uri) {
-            @Override
-            public void onOpen() {
-                Log.i("WebSocket", "Session is starting");
-                webSocketClient.send("Hello World!");
-            }
-
-            @Override
-            public void onTextReceived(String s) {
-                Log.i("WebSocket", "Message received");
-                final String message = s;
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public void onBinaryReceived(byte[] data) {
-            }
-
-            @Override
-            public void onPingReceived(byte[] data) {
-            }
-
-            @Override
-            public void onPongReceived(byte[] data) {
-            }
-
-            @Override
-            public void onException(Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-            @Override
-            public void onCloseReceived() {
-                Log.i("WebSocket", "Closed ");
-                System.out.println("onCloseReceived");
-            }
-        };
-        webSocketClient.setConnectTimeout(10000);
-        webSocketClient.setReadTimeout(60000);
-        webSocketClient.enableAutomaticReconnection(5000);
-        webSocketClient.connect();
-    }
-
-    public void sendMessage(View view){
-        Log.i("WebSocket", "Button was clicked");
-        // Send button id string to WebSocket Server
-        switch(view.getId()) {
-            case (R.id.imgBtnGui):
-                webSocketClient.send("12345");
-                break;
-        }
-    }
+//    private void createWebSocketClient() {
+//        URI uri;
+//        try {
+//            uri = new URI("ws://10.0.2.2:8080/websocket");
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        webSocketClient = new WebSocketClient(uri) {
+//            @Override
+//            public void onOpen() {
+//                Log.i("WebSocket", "Session is starting");
+//                webSocketClient.send("Hello World!");
+//            }
+//
+//            @Override
+//            public void onTextReceived(String s) {
+//                Log.i("WebSocket", "Message received");
+//                final String message = s;
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        try {
+//
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onBinaryReceived(byte[] data) {
+//            }
+//
+//            @Override
+//            public void onPingReceived(byte[] data) {
+//            }
+//
+//            @Override
+//            public void onPongReceived(byte[] data) {
+//            }
+//
+//            @Override
+//            public void onException(Exception e) {
+//                System.out.println(e.getMessage());
+//            }
+//
+//            @Override
+//            public void onCloseReceived() {
+//                Log.i("WebSocket", "Closed ");
+//                System.out.println("onCloseReceived");
+//            }
+//        };
+//        webSocketClient.setConnectTimeout(10000);
+//        webSocketClient.setReadTimeout(60000);
+//        webSocketClient.enableAutomaticReconnection(5000);
+//        webSocketClient.connect();
+//    }
+//
+//    public void sendMessage(View view){
+//        Log.i("WebSocket", "Button was clicked");
+//        // Send button id string to WebSocket Server
+//        switch(view.getId()) {
+//            case (R.id.imgBtnGui):
+//                webSocketClient.send("12345");
+//                break;
+//        }
+//    }
     @Override
     public void onClick(View v) {
         String message = mEditNhap.getText().toString().trim();
